ee300dc13764bf1c5e39f5106322f584015d9d06
juleenek <leeenek0@gmail.com>
2024-05-27 15:57:57 +0200

    Basic character display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import { useGetDisneyCharactersQuery } from './api/disney.api';
+
 const App = () => {
-  return <main></main>;
+  const [page, setPage] = useState(1);
+  const { data: characters, error, isLoading } = useGetDisneyCharactersQuery(page);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error fetching characters</div>;
+
+  return (
+    <main>
+      {characters && characters.data ? (
+        characters.data.map(({ _id, name }) => (
+          <div key={_id}>
+            {name}
+          </div>
+        ))
+      ) : (
+        <div>No characters found</div>
+      )}
+      <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+        Previous
+      </button>
+      <button onClick={() => setPage(page + 1)}>
+        Next
+      </button>
+    </main>
+  );
 };
 
 export default App;
--- src/api/disney.api.ts
+++ src/api/disney.api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { DisneyCharacter } from '../models/types';
 
-const BASE_URL = 'https://api.disneyapi.dev';
+const BASE_URL = 'https://api.disneyapi.dev/';
 
 interface ListResponse<T> {
   page: number;
@@ -15,8 +15,8 @@ export const disneyApi = createApi({
   reducerPath: 'disneyApi',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (build) => ({
-    getDisneyCharacters: build.query<ListResponse<DisneyCharacter>, number>({
-      query: (page = 1) => `character?page=${page}`,
+    getDisneyCharacters: build.query<ListResponse<DisneyCharacter>, number | void>({
+      query: (page = 1) => `character?films&page=${page}`,
     }),
   }),
 });
